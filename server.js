984d3b3c111568c49de5ac6092ffdd512f0dff27
Vlad Duta <vlad.du09@gmail.com>
2017-07-24 18:38:25 +0000

    Added SASS features

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
-// const sass        = require("node-sass-middleware");
+const sass        = require("node-sass-middleware");
 const cookieSession = require('cookie-session');
 const bcrypt      = require('bcrypt');
 const app         = express();
@@ -39,12 +39,12 @@ app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use("/styles", sass({
-//   src: __dirname + "/styles",
-//   dest: __dirname + "/public/styles",
-//   debug: true,
-//   outputStyle: 'expanded'
-// }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
 app.use(express.static("public"));
 
 // Mounts all resource routes
