3ec78fa47ab2b71d3832c5b03876eca8552b753c
JialunC <jialun.tom.chen@gmail.com>
2017-07-24 18:14:58 +0000

    Cleaned up codes in server.js file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
+// const sass        = require("node-sass-middleware");
 const cookieSession = require('cookie-session');
 const bcrypt      = require('bcrypt');
 const app         = express();
@@ -16,24 +16,25 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
-// Seperated Routes for each Resource
+// Seperates Routes for each resource
 const itemsRoutes = require("./routes/items.js");
 const searchRoutes = require("./routes/search.js");
 const checkUserRoutes = require("./routes/checkuser.js");
 const registerRoutes = require("./routes/register.js");
 const loginRoutes = require("./routes/login.js");
 const profileRoutes = require("./routes/profile.js");
+
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
 }));
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// Loads the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
-// Log knex SQL queries to STDOUT as well
+// Logs knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
@@ -46,17 +47,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // }));
 app.use(express.static("public"));
 
-// Mount all resource routes
+// Mounts all resource routes
 app.use("/api/items", itemsRoutes(knex));
 app.use("/api/search", searchRoutes(knex));
 app.use("/api/checkuser", checkUserRoutes(knex));
 app.use("/api/register", registerRoutes(knex));
 app.use("/api/login", loginRoutes(knex));
 app.use("/api/profile", profileRoutes(knex));
-// Home page
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
