56f90f1945564c4ce91d35fc043fab7e95ef7149
JialunC <jialun.tom.chen@gmail.com>
2017-08-06 16:06:59 +0000

    Added /lib/utils.js to store common codes

--- routes/login.js
+++ routes/login.js
@@ -4,19 +4,21 @@ const express = require('express');
 const router  = express.Router();
 const bcrypt = require('bcrypt');
 
-function generateRandomString() {
-  var myArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var myString = "";
-  for (var i = 0; i < 6; i++) {
-    myString += myArray[Math.floor(Math.random() * myArray.length)];
-  }
-  return myString;
-}
+const Utils = require('../lib/utils.js')
+
+// function generateRandomString() {
+//   var myArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//   var myString = "";
+//   for (var i = 0; i < 6; i++) {
+//     myString += myArray[Math.floor(Math.random() * myArray.length)];
+//   }
+//   return myString;
+// }
 
 module.exports = (knex) => {
 
   router.post("/", (req, res) => {
-    let user_id = generateRandomString()
+    let user_id = Utils.generateRandomString()
     req.session.user_id = user_id
     knex
     .select("password")
